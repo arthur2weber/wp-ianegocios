879c3cd3155d4b729b96f5effb5f76ad4d8a3b3c
PC\Asus <arthur2weber@gmail.com>
2020-04-19 16:51:56 -0300

    inicial
    inicial

---
+++ index.js
@@ -0,0 +1,26 @@
+//import { create, Whatsapp } from 'sulla';
+const create = require("sulla").create;
+const Whatsapp = require("sulla").Whatsapp;
+create().then(client => start(client));
+
+
+function start(client) {
+  client.onMessage(message => {
+    if (message.body.length>0 && message.isGroupMsg==false) {
+		
+	const currentHour = new Date().getHours();
+
+	const greetingMessage =
+	  currentHour >= 5 && currentHour < 12 ? // after 4:00AM and before 12:00PM
+	  'Bom dia' :
+	  currentHour >= 12 && currentHour <= 18 ? // after 12:00PM and before 6:00pm
+	  'Boa tarde' :
+	  currentHour > 18 || currentHour < 5 ? // after 5:59pm or before 4:00AM (to accommodate night owls)
+	  'Boa noite' : // if for some reason the calculation didn't work
+	  'Olá'
+
+	console.log(greetingMessage)
+		client.sendText(greetingMessage+' '+message.sender.pushname+'! somos a Lalala Lanches, Acesse no link a seguir nosso sistema de pedidos: https://gg.gg/Lalala_pedidos');
+    }	
+  });
+}
