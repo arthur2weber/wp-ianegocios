9eda7dafa77413132ecdab9d36347eace65c018c
PC\Asus <arthur2weber@gmail.com>
2020-04-23 21:37:49 -0300

    começando a usar

--- functions/app/index.js
+++ functions/app/index.js
@@ -17,52 +17,12 @@ export default function expressApp(functionName) {
   // Set router base path for local dev
   const routerBasePath = process.env.NODE_ENV === 'dev' ? `/${functionName}` : `/.netlify/functions/${functionName}/`
 
+
+
+
   /* define routes */
-  router.get('/', (req, res) => {
-    const html = `
-    <html>
-      <head>
-        <style>
-          body {
-            padding: 30px;
-          }
-        </style>
-      </head>
-      <body>
-        <h1>Express via '${functionName}' ⊂◉‿◉つ</h1>
-
-        <p>I'm using Express running via a <a href='https://www.netlify.com/docs/functions/' target='_blank'>Netlify Function</a>.</p>
-
-        <p>Choose a route:</p>
-
-        <div>
-          <a href='/.netlify/functions/${functionName}/users'>View /users route</a>
-        </div>
-
-        <div>
-          <a href='/.netlify/functions/${functionName}/hello'>View /hello route</a>
-        </div>
-
-        <br/>
-        <br/>
-
-        <div>
-          <a href='/'>
-            Go back to demo homepage
-          </a>
-        </div>
-
-        <br/>
-        <br/>
-
-        <div>
-          <a href='https://github.com/DavidWells/netlify-functions-express' target='_blank'>
-            See the source code on github
-          </a>
-        </div>
-      </body>
-    </html>
-  `
+  router.get('/:num/start', (req, res) => {
+    const html = ` n =  `+req.params.num;
     res.send(html)
   })
 
