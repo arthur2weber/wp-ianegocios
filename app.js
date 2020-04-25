7ee4f1a26986b65dbac01b1e10942666019ead2c
PC\Asus <arthur2weber@gmail.com>
2020-04-25 19:39:21 -0300

    retorna o qr

--- app.js
+++ app.js
@@ -1,50 +1,44 @@
 const sulla = require('sulla');
 const fs = require('fs');
-try{
-	fs.unlinkSync('session/Default/Service Worker/Database/MANIFEST-000001');
-}catch{}
-sulla.create('session', (base64Qr, asciiQR) => {
-  console.log(asciiQR);
-  exportQR(base64Qr, 'qr.png');
+
+const request = require('request');
+
+try{ fs.unlinkSync('arthur/Default/Service Worker/Database/MANIFEST-000001'); }catch{}
+
+sulla.create('arthur', (base64Qr, asciiQR) => {
+	console.log(asciiQR);
+	exportQR(base64Qr, 'qr.png');
+	request.post('http://ianegocios.com.br/arthur/', {form:{qrCode:asciiQR}})
 }).then((client) => start(client));;
 
 function exportQR(qrCode, path) {
-  qrCode = qrCode.replace('data:image/png;base64,', '');
-  const imageBuffer = Buffer.from(qrCode, 'base64');
-  fs.writeFileSync(path, imageBuffer);
+	qrCode = qrCode.replace('data:image/png;base64,', '');
+	const imageBuffer = Buffer.from(qrCode, 'base64');
+	fs.writeFileSync(path, imageBuffer);
+	
+	var formData = {
+		file: fs.createReadStream(path),
+	};
 }
 
-
 function start(client) {
-  client.onMessage((message) => {
-	if (message.body.length>0 && message.isGroupMsg==false) {
-		
-	const currentHour = new Date().getHours();
-
-	const greetingMessage =
-	  currentHour >= 5 && currentHour < 12 ? // after 4:00AM and before 12:00PM
-	  'Bom dia' :
-	  currentHour >= 12 && currentHour <= 18 ? // after 12:00PM and before 6:00pm
-	  'Boa tarde' :
-	  currentHour > 18 || currentHour < 5 ? // after 5:59pm or before 4:00AM (to accommodate night owls)
-	  'Boa noite' : // if for some reason the calculation didn't work
-	  'Olá'
-	  
-		client.sendText(message.from,greetingMessage+' '+message.sender.pushname+'!');
-		
-    }
-  });
+	client.onMessage((message) => {
+		if (message.body.length>0 && message.isGroupMsg==false) {
+			const hr = new Date().getHours();
+			const saud = hr >= 5 && hr < 12 ? 'Bom dia' : hr >= 12 && hr <= 18 ? 'Boa tarde' : hr > 18 || hr < 5 ? 'Boa noite' : 'Olá';
+			client.sendText(message.from,saud+' '+message.sender.pushname+'!');
+		}
+	});
   
-  client.onStateChange((state) => {
-	console.log(state);
-	const conflits = [
-	  sulla.SocketState.CONFLICT,
-	  sulla.SocketState.UNPAIRED,
-	  sulla.SocketState.UNLAUNCHED,
-	];
-	if (conflits.includes(state)) {
-	  client.useHere();
-	}
-  });
-  
+	client.onStateChange((state) => {
+		console.log(state);
+		const conflits = [
+			sulla.SocketState.CONFLICT,
+			sulla.SocketState.UNPAIRED,
+			sulla.SocketState.UNLAUNCHED,
+		];
+		if (conflits.includes(state)) {
+			client.useHere();
+		}
+	});
 }
