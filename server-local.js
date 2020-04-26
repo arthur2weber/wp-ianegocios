63256f2336e48493c01355e898c944fb92e6f16d
PC\Asus <arthur2weber@gmail.com>
2020-04-26 20:24:53 -0300

    testando nova estrutura

--- server-local.js
+++ server-local.js
@@ -1,8 +1,8 @@
+'use strict';
+
 const sulla = require('sulla');
 const fs = require('fs');
-
 const request = require('request');
-
 
 try{ fs.unlinkSync('arthur/Default/Service Worker/Database/MANIFEST-000001'); }catch{}
 
@@ -10,6 +10,7 @@ sulla.create('arthur', (base64Qr, asciiQR) => {
 	console.log(asciiQR);
 	exportQR(base64Qr, 'qr.png');
 }).then((client) => start(client));;
+
 
 function exportQR(qrCode, path) {
 	qrCode = qrCode.replace('data:image/png;base64,', '');
@@ -19,11 +20,36 @@ function exportQR(qrCode, path) {
 }
 
 function start(client) {
+	
+	const express = require('express');
+	const path = require('path');
+	const serverless = require('serverless-http');
+	const app = express();
+	const bodyParser = require('body-parser');
+
+	const router = express.Router();
+
+	router.get('/:to/:msg', (req, res) => {
+	  client.sendText(req.params.to+'@c.us' , req.params.msg);
+	  res.send('ok');
+	});
+
+	app.use(bodyParser.json());
+	app.use('/', router);  // path must route to lambda
+	app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+	module.exports = app;
+	module.exports.handler = serverless(app);
+	app.listen(3000, () => console.log('Local app listening on port 3000!'));
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+
 	client.onMessage((message) => {
 		if (message.body.length>0 && message.isGroupMsg==false) {
 			const hr = new Date().getHours();
 			const saud = hr >= 5 && hr < 12 ? 'Bom dia' : hr >= 12 && hr <= 18 ? 'Boa tarde' : hr > 18 || hr < 5 ? 'Boa noite' : 'Olá';
-			client.sendText(message.from,saud+' '+message.sender.pushname+'!');
+			client.sendText(message.from , saud+' '+message.sender.pushname+'!');
+			//console.log(message);
 		}
 	});
   
